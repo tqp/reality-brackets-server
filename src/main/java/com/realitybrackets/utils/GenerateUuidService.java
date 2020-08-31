98b9a25a57cf8a5bcadb01b93d84f1176d9d96e5
Tim <timothyqpowers@gmail.com>
2020-08-31 15:02:21 -0400

    Added Utils

---
+++ src/main/java/com/realitybrackets/utils/GenerateUuidService.java
@@ -0,0 +1,20 @@
+package com.realitybrackets.utils;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class GenerateUuidService {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++) {
+            GenerateUuidService generateUuidService = new GenerateUuidService();
+            System.out.println(generateUuidService.GenerateUuid());
+        }
+    }
+
+    public String GenerateUuid() {
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase();
+    }
+}
